5c367003281fad8d70757876fcbb544171a8dfc5
yellowisk <heitor.a@aluno.ifsp.edu.br>
2024-01-02 21:48:30 -0300

    project setup

---
+++ src/main/java/br/com/dopad/DoPadApplication.java
@@ -0,0 +1,13 @@
+package br.com.dopad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoPadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dopad/DoPadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dopad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoPadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
