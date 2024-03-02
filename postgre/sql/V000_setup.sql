3d67d5b6e84b6ef7a99101451cb3f774eb957f66
yellowisk <heitor.a@aluno.ifsp.edu.br>
2024-03-02 19:05:27 -0300

    chore: add database creation in setup sql file

--- postgre/sql/V000_setup.sql
+++ postgre/sql/V000_setup.sql
@@ -3,3 +3,5 @@ CREATE USER "dopad-ro" WITH PASSWORD 'dopad-ro' IN ROLE "dopad";
 CREATE USER "dopad-dml" WITH PASSWORD 'dopad-dml' IN ROLE "dopad";
 CREATE USER "dopad-app" WITH PASSWORD 'dopad-app' IN ROLE "dopad";
 ALTER USER "dopad-app" SET search_path = public,dopad_platform;
+
+CREATE DATABASE "dopad" WITH OWNER = "dopad-app";
